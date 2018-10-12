537900b4660be3ca86961dbb3c45a289f3abc54e
Katrina Pritchard <pritckat@gmail.com>
2018-10-12 18:21:53 +0000

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 function lowerCaseDrivers (drivers) {
   return drivers.map(function(name) {
     return name.toLowerCase();
-  })
+  });
 }
 
 function nameToAttributes(drivers) {
-  const newDrivers = drivers.map(function (driver) {
-    return Object.assign({}, driver, { firstName: 'Bobby', lastName: 'Hello'
+  return drivers.map(function(driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return { firstName: driverFirst, lastName: driverLast };
   });
-})
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
