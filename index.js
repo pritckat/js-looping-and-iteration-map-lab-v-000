0713bd8cd267a065dad9b4119798f052e9991664
Katrina Pritchard <pritckat@gmail.com>
2018-10-12 18:21:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function lowerCaseDrivers (drivers) {
   return drivers.map(function(name) {
     return name.toLowerCase();
-  })
+  });
 }
 
 function nameToAttributes(drivers) {
@@ -10,5 +10,11 @@ function nameToAttributes(drivers) {
     const driverFirst = driver.split(' ')[0];
     const driverLast = driver.split(' ')[1];
     return { firstName: driverFirst, lastName: driverLast };
-  })
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
 }
